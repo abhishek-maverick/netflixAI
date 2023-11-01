9a4edd2403c829bf4d78365f7fbef5d21e888d31
abhishek-maverick <abhishekranjan0511@gmail.com>
2023-11-02 01:36:05 +0530

    feat: added custom hooks and video background

---
+++ src/components/VideoTitle.js
@@ -0,0 +1,18 @@
+const VideoTitle = ({ title, overview }) => {
+  return (
+    <div className="px-24 absolute text-white bg-gradient-to-r from-black w-screen aspect-video pt-[20%]">
+      <h1 className="text-6xl font-bold">{title}</h1>
+      <p className="py-6 text-lg w-1/4">{overview}</p>
+      <div className="">
+        <button className="hidden md:inline-block mx-2  bg-white text-black p-4 px-12 text-xl rounded-lg hover:bg-opacity-80">
+          ▶️ Play
+        </button>
+        <button className="hidden md:inline-block mx-2  bg-gray-500 text-white p-4 px-12 text-xl bg-opacity-50 rounded-lg">
+          ℹ️ More Info
+        </button>
+      </div>
+    </div>
+  );
+};
+
+export default VideoTitle;
---
+++ src/hooks/useMovieTrailer.js
@@ -0,0 +1,22 @@
+import { useEffect } from "react";
+import { useDispatch } from "react-redux";
+import { API_OPTIONS, API_TRAILER_URL } from "../utils/constants";
+import { addTrailerVideo } from "../utils/moviesSlice";
+
+const useMovieTrailer = (movieId) => {
+  const dispatch = useDispatch();
+  useEffect(() => {
+    getMoviesVideos();
+  }, []);
+  const getMoviesVideos = async () => {
+    const data = await fetch(API_TRAILER_URL(movieId), API_OPTIONS);
+    const json = await data.json();
+    console.log(json);
+    const filterData = json.results.filter((video) => video.type === "Trailer");
+    const trailer = filterData.length ? filterData[0] : json.results[0];
+    console.log(trailer);
+    dispatch(addTrailerVideo(trailer));
+  };
+};
+
+export default useMovieTrailer;
