b1ff4ed7479a6b57365e12c68be96a921f0aa54a
abhishek-maverick <abhishekranjan0511@gmail.com>
2023-11-04 23:59:01 +0530

    chore: added languages support

--- src/components/GptSearchBar.js
+++ src/components/GptSearchBar.js
@@ -1,3 +1,6 @@
+import { SUPPORTED_LANGUAGES } from "../utils/constants";
+import lang from "../utils/languageConstants";
+
 const GptSearchBar = () => {
   return (
     <div className="pt-[8%] flex justify-center">
@@ -5,10 +8,17 @@ const GptSearchBar = () => {
         <input
           type="text"
           className="p-4 m-4 col-span-9"
-          placeholder="What would you like to watch today ?"
+          placeholder={lang.hindi.gptSearchPlaceHolder}
         />
+        <select className="p-2 m-2 bg-gray-500 text-white">
+          {SUPPORTED_LANGUAGES.map((lang) => (
+            <option key={lang.identifier} value={lang.identifier}>
+              {lang.name}
+            </option>
+          ))}
+        </select>
         <button className="py-2 px-4 m-4 bg-red-700 text-white rounded-lg col-span-3">
-          Search{" "}
+          {lang.hindi.search}
         </button>
       </form>
     </div>
