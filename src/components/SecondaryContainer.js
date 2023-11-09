3a4cbf1fb180205766244eb97b1eb87683262ea2
abhishek-maverick <abhishekranjan0511@gmail.com>
2023-11-09 23:51:29 +0530

    chore: made main-container responsive

--- src/components/SecondaryContainer.js
+++ src/components/SecondaryContainer.js
@@ -6,7 +6,7 @@ const SecondaryContainer = () => {
   return (
     movies.nowPlayingMovies && (
       <div className=" bg-black">
-        <div className="-mt-[350px] relative z-20 pl-12">
+        <div className="mt-0 md:-mt-[350px] relative z-20 pl-12">
           <MovieList title={"Now playing"} movies={movies?.nowPlayingMovies} />
           <MovieList title={"Popular"} movies={movies?.popularMovies} />
           <MovieList title={"Top Rated"} movies={movies?.topRatedMovies} />
--- src/components/VideoTitle.js
+++ src/components/VideoTitle.js
@@ -1,8 +1,8 @@
 const VideoTitle = ({ title, overview }) => {
   return (
     <div className="px-24 absolute text-white bg-gradient-to-r from-black w-screen aspect-video pt-[8%]">
-      <h1 className="text-6xl font-bold">{title}</h1>
-      <p className="py-6 text-lg w-1/4">{overview}</p>
+      <h1 className=" text-2xl md:text-6xl font-bold">{title}</h1>
+      <p className="py-6 text-lg w-1/4 hidden md:inline-block">{overview}</p>
       <div className="">
         <button className="hidden md:inline-block mx-2  bg-white text-black p-4 px-12 text-xl rounded-lg hover:bg-opacity-80">
           ▶️ Play
